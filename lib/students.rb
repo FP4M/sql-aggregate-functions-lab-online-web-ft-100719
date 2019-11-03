e32961443524e38dee46c2d66d930826d3bf94e3
Prince Charles <princecharles35@yahoo.com>
2019-11-03 20:41:36 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,10 +1,3 @@
-CREATE TABLE students
-    (id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER,
-    gpa FLOAT,
-    tardies INTEGER);
-
 def highest_student_gpa
   "SELECT MAX(students.gpa) FROM students";
 end
